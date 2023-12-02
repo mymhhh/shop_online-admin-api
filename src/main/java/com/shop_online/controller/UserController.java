d20ddde4a7ec8a26f405c5220fabaf6d94029d8c
mym <3216187300@qq.com>
2023-12-02 19:20:54 +0800

    后端管理

---
+++ src/main/java/com/shop_online/controller/UserController.java
@@ -0,0 +1,51 @@
+package com.shop_online.controller;
+
+import com.shop_online.common.model.Query;
+import com.shop_online.common.result.PageResult;
+import com.shop_online.common.result.Result;
+import com.shop_online.service.UserService;
+import com.shop_online.vo.UserVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author zero
+ * @since 2023-11-28
+ */
+@Api(tags = "用户管理")
+@RestController
+@RequestMapping("user")
+@AllArgsConstructor
+public class UserController {
+    private final UserService userService;
+
+    @PostMapping("page")
+    @ApiOperation("分页")
+    @PreAuthorize("hasAnyAuthority('sys:user:list')")
+    public Result<PageResult<UserVO>> page(@RequestBody @Valid Query query) {
+        PageResult<UserVO> result = userService.getPage(query);
+        return Result.ok(result);
+    }
+
+    @PostMapping("export")
+    @ApiOperation("导出用户信息")
+    public Result<String> export(@RequestBody Query query, HttpServletResponse response) {
+        userService.exportUserInfo(query, response);
+        return Result.ok();
+    }
+
+}
